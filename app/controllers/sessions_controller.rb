1aacd94a08ea5e6b4991f25031d21270cb94f639
Tanner Steward & Andrew Akers <student@epicodus.com>
2014-09-11 15:46:24 -0700

    ajax works for sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,20 @@
 class SessionsController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
     user = User.find_by_name(params[:name])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_path, notice: 'You have been logged in successfully!'
+      respond_to do |format|
+        format.html { redirect_to root_path, notice: 'You have been logged in successfully!' }
+        format.js
+      end
     else
       flash.now.alert = 'email of password is invalid'
-      render 'new'
+      render "new"
     end
   end
 
